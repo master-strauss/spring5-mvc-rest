861b29421de836c50a7d7db89c07971acac4d73a
MasterStrauss <blueplanet19x@gmail.com>
2021-10-19 00:17:53 +0100

    adding category service

--- src/main/java/guru/springfamework/api/v1/mapper/CategoryMapper.java
+++ src/main/java/guru/springfamework/api/v1/mapper/CategoryMapper.java
@@ -3,6 +3,7 @@ package guru.springfamework.api.v1.mapper;
 import guru.springfamework.api.v1.model.CategoryDTO;
 import guru.springfamework.domain.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 /**
@@ -12,7 +13,7 @@ import org.mapstruct.factory.Mappers;
 public interface CategoryMapper {
 
     CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
-
-
+    
+    @Mapping(source = "id", target="id")
     CategoryDTO categoryToCategoryDTO(Category category);
 }
